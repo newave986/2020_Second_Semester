31819a49a1b6ac897e0efdf21ce58454c93f2236
newave986 <kingofthefall538@gmail.com>
2021-03-09 13:29:18 +0900

    calculate the area of circle

---
+++ 2021-1 Object-Oriented Programming/09-03-21/CircleArea.java
@@ -0,0 +1,12 @@
+public class CircleArea {
+
+    public static void main(String[] args) {
+        final double PI = 3.14; // define the circumference rate as a constant
+
+        double radius = 10.0; // radius of circle
+        double circleArea = radius*radius*PI; // calculate the area of circle
+
+        System.out.println("Area of Given Circle = " + circleArea);
+        // print the area of circle out to screen
+    }
+}

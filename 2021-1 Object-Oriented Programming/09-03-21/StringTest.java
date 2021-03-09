f093a27eaa15ddb900a880388fa51bc91f09e727
newave986 <kingofthefall538@gmail.com>
2021-03-09 13:30:48 +0900

    test string type

---
+++ 2021-1 Object-Oriented Programming/09-03-21/StringTest.java
@@ -0,0 +1,10 @@
+class StringTest  {
+	String str = "Good";
+	// String is like class
+
+	String toolName = "JDK";
+	toolName + 1.8 // "JDK1.8"
+	"("+3+","+5+")" // "(3,5)"
+	System.out.println(toolName + " out now"); // print "JDK1.8 out now"
+}
+

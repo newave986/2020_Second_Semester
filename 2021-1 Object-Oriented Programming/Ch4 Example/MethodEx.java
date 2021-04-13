21b3f841bbf0115600544d9688a9c4515c549840
newave986 <kingofthefall538@gmail.com>
2021-04-13 12:29:31 +0900

    Chapter 4 Example - 0409 - Method & Overloading

--- 2021-1 Object-Oriented Programming/Ch4 Example/MethodEx.java
+++ 2021-1 Object-Oriented Programming/Ch4 Example/MethodEx.java
@@ -1,3 +1,11 @@
+/*
+메소드 구현 & 메소드 오버로딩
+하나의 클래스 안에 같은 이름의 메소드를 여러 개 선언해도 문제가 없다.
+
+단, 매개변수 갯수가 다르거나, 매개변수의 타입이 달라야 한다.
+- 자바의 다형성 특징
+*/
+
 public class MethodEx {
 
     public int getSum(int i, int j) {
@@ -15,7 +23,7 @@ public class MethodEx {
     public static void main(String[] args) {
         MethodEx x = new MethodEx();
         System.out.println(x.getSum(1, 2));
-        System.out.println(x.getSum2(1));
+        System.out.println(x.getSum(1));
     }
 
 }

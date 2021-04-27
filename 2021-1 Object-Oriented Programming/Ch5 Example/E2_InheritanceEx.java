1ac9cebdef131b309e7166f1f7b1b7428f445a9f
newave986 <kingofthefall538@gmail.com>
2021-04-27 12:37:32 +0900

    Chapter 5 Example - 0427 - 상속 관계에 있는 클래스 간 멤버 접근

--- 2021-1 Object-Oriented Programming/Ch5 Example/E2_InheritanceEx.java
+++ 2021-1 Object-Oriented Programming/Ch5 Example/E2_InheritanceEx.java
@@ -1,5 +1,30 @@
+class Person {
+    private int weight;
+    int age;
+    protected int height;
+    public String name;
 
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+    public int getWeight() {
+        return weight;
+    }
+}
+
+class Student extends Person {
+    public void set() {
+        age = 30;
+        name = "홍길동";
+        height = 175;
+        // weight = 99; // 오류. 수퍼 클래스의 private 접근 불가
+        setWeight(99); // private 멤버 weight는 setWeight()으로 간접 접근
+    }
+}
 
 public class E2_InheritanceEx {
-    
+    public static void main(String[] args) {
+        Student s = new Student();
+        s.set();
+    }
 }
